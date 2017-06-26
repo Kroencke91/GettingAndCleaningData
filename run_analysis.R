639a0865ec0358685c300b1fce081ff1edff66da
Kroencke91 <kroencke91@gmail.com>
2017-06-25 21:51:04 -0500

    Final commit.

--- run_analysis.R
+++ run_analysis.R
@@ -1,9 +1,10 @@
+setwd("C:\\Users\\admin\\Documents\\R\\GettingAndCleaningData\\CourseProject\\GettingAndCleaningData")
+
 ##############################################################
 #Packages setup
 ##############################################################
-require(dplyr)
 
-require(lubridate)
+require(dplyr)
 
 require(tidyr)
 
@@ -14,15 +15,14 @@ require(data.table)
 ##############################################################
 #Initialize
 ##############################################################
-rootdir <- "C:\\Users\\admin\\Documents\\R\\GettingAndCleaningData\\UCI_HAR_Dataset\\"
 
-activitylabels <- tbl_df(read.delim2(paste0(rootdir,"activity_labels.txt"),header = FALSE, sep=" "))
+activitylabels <- tbl_df(read.delim2("activity_labels.txt",header = FALSE, sep=" "))
 
 names(activitylabels) <- c("actid","activity")
 
 activitylabels <- select(activitylabels, activity, actid)
 
-features <- tbl_df(read.delim2(paste0(rootdir,"features.txt"),header = FALSE, sep=" "))
+features <- tbl_df(read.delim2("features.txt",header = FALSE, sep=" "))
 
 names(features) <- c("index","feature")
 
@@ -31,36 +31,34 @@ features <- mutate(features,colname=make.unique(tolower(gsub("[(),-]","",feature
 ##############################################################
 #TEST dataset setup
 ##############################################################
-testdir <- paste0(rootdir,"test\\")
 
-testsubject <- tbl_df(read.delim2(paste0(testdir,"subject_test.txt"),header = FALSE))
+testsubject <- tbl_df(read.delim2("subject_test.txt",header = FALSE))
 
 names(testsubject) <- c("subject")
 
-testdat <- readLines(paste0(testdir,"X_test.txt")) %>%
+testdat <- readLines("X_test.txt") %>%
   tbl_df() %>%
   separate(value,c(features$colname),sep=" ") %>%
   tbl_df()
 
-testlabs <- tbl_df(read.delim2(paste0(testdir,"y_test.txt"),header = FALSE))  
+testlabs <- tbl_df(read.delim2("y_test.txt",header = FALSE))  
 
 names(testlabs) <- c("actid")
 
 ##############################################################
 #TRAIN dataset setup
 ##############################################################
-traindir <- paste0(rootdir,"train\\")
 
-trainsubject <- tbl_df(read.delim2(paste0(traindir,"subject_train.txt"),header = FALSE))
+trainsubject <- tbl_df(read.delim2("subject_train.txt",header = FALSE))
 
 names(trainsubject) <- c("subject")
 
-traindat <- readLines(paste0(traindir,"X_train.txt")) %>%
+traindat <- readLines("X_train.txt") %>%
   tbl_df() %>%
   separate(value,c(features$colname),sep=" ") %>%
   tbl_df()
 
-trainlabs <- tbl_df(read.delim2(paste0(traindir,"y_train.txt"),header = FALSE))  
+trainlabs <- tbl_df(read.delim2("y_train.txt",header = FALSE))  
 
 names(trainlabs) <- c("actid")
 
@@ -117,6 +115,22 @@ names(meanstdmeans) <- c(names(meanstdmeans)[1:3],
                          apply(tbl_df(names(meanstdmeans))[-(1:3),], 2, paste0, "mean")[,1])
 
 View(meanstdmeans)
+
+##############################################################
+#Write files
+##############################################################
+
+x <- tbl_df(names(meanstddat))
+
+names(x) <- c("colname")
+
+z <- subset(features, colname %in% x$colname)
+
+write.table(z,"featuretocolnamemapping.txt", quote = FALSE, row.names = FALSE, sep = "\t|\t", eol = "\n\r")
+
+write.table(meanstddat, "meanstddat.txt", quote = FALSE, row.names = FALSE, sep = "\t")
+
+write.table(meanstdmeans, "meanstdmeans.txt", quote = FALSE, row.names = FALSE, sep = "\t")
 
 ##############################################################
 #End of script
